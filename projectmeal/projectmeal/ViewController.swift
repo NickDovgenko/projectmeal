9c0e83ed27f2529d97c29bab2338b0af850643fe
Nick <nick@MacBook-Pro-Nick.local>
2016-08-15 21:47:06 +0300

    Main view 1.01

--- projectmeal/projectmeal/ViewController.swift
+++ projectmeal/projectmeal/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var ImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Анимация стрелок на главном экране
+        //Левая стрелка
+        ImageView.animationImages = [UIImage(named: "Image1-4")!,UIImage(named: "Image1-5")!,UIImage(named: "Image1-6")!,UIImage(named: "Image1-7")!,UIImage(named: "Image1-7")!,UIImage(named: "Image1-6")!,UIImage(named: "Image1-5")!,UIImage(named: "Image1-4")!]
+        ImageView.animationDuration = 1.2
+        ImageView.startAnimating()
+        
+        //Правая стрелка
+        ImageView2.animationImages = [UIImage(named: "Image2-4")!,UIImage(named: "Image2-5")!,UIImage(named: "Image2-6")!,UIImage(named: "Image2-7")!,UIImage(named: "Image2-7")!,UIImage(named: "Image2-6")!,UIImage(named: "Image2-5")!,UIImage(named: "Image2-4")!]
+        ImageView2.animationDuration = 1.2
+        ImageView2.startAnimating()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
